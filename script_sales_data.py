7f5123a60c2f4de8172e4d0ad675a23a9c9c0ef3
Pamela de Souza Bella Guerra <40528996+pamsbg@users.noreply.github.com>
2023-11-26 21:28:05 -0300

    atualização teste unitario

--- script_sales_data.py
+++ script_sales_data.py
@@ -13,110 +13,175 @@ import pandas as pd
 from sqlalchemy import create_engine 
 import logging
 
+
+def check_unicidade(sales_data):
+
+    try:
+        #checar se todos os ids são unicos
+        ids_unicos = sales_data['transaction_id'].nunique()
+        if ids_unicos < sales_data.shape[0]:
+            sales_data = sales_data.drop_duplicates(subset='transaction_id', keep='first')
+        
+           
+        return sales_data
+    except Exception as e:
+            # registrar a exceção
+            logging.exception(f'Ocorreu um erro: {e}')
+            
+def check_duplicidade(sales_data):
+
+    try:
+        #checar se tem linhas duplicadas
+        duplicados = sales_data[sales_data.duplicated()]
+        
+        #remover duplicados
+        if duplicados.shape[0] > 0:
+            sales_data = sales_data.drop_duplicates(subset='transaction_id', keep='first')
+       
+              
+        return sales_data
+    except Exception as e:
+        # registrar a exceção
+        logging.exception(f'Ocorreu um erro: {e}')
+
+def check_vazios(sales_data):
+
+    try:
+        #checar se tem linhas vazias
+        linhas_vazias = sales_data["sale_value"].isna().sum()
+        
+    
+        #remover linhas vazias
+        if linhas_vazias > 0:
+            sales_data = sales_data.dropna(how="any", axis=0)
+            
+            
+        return sales_data
+    except Exception as e:
+        # registrar a exceção
+        logging.exception(f'Ocorreu um erro: {e}')
+
+def check_negativos(sales_data):
+
+    try:    
+        #Confirme se os valores de vendas não são negativos.
+        positivos = sales_data["sale_value"].where(sales_data["sale_value"] >= 0).shape[0]
+        total = sales_data.shape[0]
+        negativos = total - positivos
+        if negativos > 0:
+            print("Há " + str(negativos) + " valores negativos. Observe as linhas abaixo")
+            print(sales_data.where(sales_data["sale_value"] < 0))
+        else:
+            print("não há números negativos na coluna sale_value")
+        
+        return sales_data
+    except Exception as e:
+        # registrar a exceção
+        logging.exception(f'Ocorreu um erro: {e}')
+
+def check_data(sales_data):
+    try:    
+        #verificar se a coluna date é timestamp e converter se não
+        é_timestamp = pd.core.dtypes.common.is_datetime_or_timedelta_dtype(sales_data)
+        
+        if é_timestamp == False:
+            sales_data['date'] = pd.to_datetime(sales_data['date'])
+           
+        return sales_data
+    except Exception as e:
+        # registrar a exceção
+        logging.exception(f'Ocorreu um erro: {e}')
+
 def limpeza_dados(sales_data):
-
-    # checar formato dos dados    
-    sales_data.info()
+    try:
     
-    #checar se todos os ids são unicos
-    ids_unicos = sales_data['transaction_id'].nunique()
-    if ids_unicos < sales_data.shape[0]:
-        sales_data = sales_data.drop_duplicates(subset='transaction_id', keep='first')
-    
-    
-    #checar se tem linhas duplicadas
-    duplicados = sales_data[sales_data.duplicated()]
-    
-    #remover duplicados
-    if duplicados.shape[0] > 0:
-        sales_data = sales_data.drop_duplicates(subset='transaction_id', keep='first')
-   
-    #checar se tem linhas vazias
-    linhas_vazias = sales_data["sale_value"].isna().sum()
-    
-
-    #remover linhas vazias
-    if linhas_vazias > 0:
-        sales_data = sales_data.dropna(how="any", axis=0)
+        # checar formato dos dados    
+        sales_data.info()
         
-    #Confirme se os valores de vendas não são negativos.
-    positivos = sales_data["sale_value"].where(sales_data["sale_value"] >= 0).shape[0]
-    total = sales_data.shape[0]
-    negativos = total - positivos
-    if negativos > 0:
-        print("Há " + str(negativos) + " valores negativos. Observe as linhas abaixo")
-        print(sales_data.where(sales_data["sale_value"] < 0))
-    else:
-        print("não há números negativos na coluna sale_value")
-
-    #verificar se a coluna date é timestamp e converter se não
-    é_timestamp = pd.core.dtypes.common.is_datetime_or_timedelta_dtype(sales_data)
-    
-    if é_timestamp == False:
-        sales_data['date'] = pd.to_datetime(sales_data['date'])
-
-
+        #checar unicidade
+        sales_data_unico = check_unicidade(sales_data)
         
-    return sales_data
-
+        #checar duplicidade
+        sales_data_unico = check_duplicidade(sales_data_unico)
+        
+        #checar vazios 
+        sales_data_vazios = check_vazios(sales_data_unico)
+        
+        #checar negativos
+        sales_data_negativos = check_negativos(sales_data_vazios)
+        
+        #checar data
+        sales_data_limpo = check_data(sales_data_negativos)
+    
+        return sales_data_limpo
+    except Exception as e:
+        # registrar a exceção
+        logging.exception(f'Ocorreu um erro: {e}')
 
 def conversao (df, coluna, taxa_conversao, currency):
-    df_convertido = df.copy()
-    df_convertido[coluna] = df[coluna] * taxa_conversao
-    df_convertido['currency'] = currency
-    return df_convertido
-
+    try:
+        df_convertido = df.copy()
+        df_convertido[coluna] = df[coluna] * taxa_conversao
+        df_convertido['currency'] = currency
+        return df_convertido
+    except Exception as e:
+        # registrar a exceção
+        logging.exception(f'Ocorreu um erro: {e}')
 
 
 
 
 def criar_tabela(data):
-    contador = 0
-    # criar conexão com db
-    conn_string = 'postgres://postgres:pass@127.0.0.1/Weather_Database'
-    
-    db = create_engine(conn_string) 
-    conn = db.connect() 
-    conn1 = psycopg2.connect( 
-    	database="Sales_Database", 
-    user='postgres', 
-    password='pass', 
-    host='127.0.0.1', 
-    port= '5432'
-    ) 
-    
-    conn1.autocommit = False
-    cursor = conn1.cursor() 
-    
-    # deletar tabela se ela já existir
-    cursor.execute('drop table if exists sales_info') 
-    
-    #criar tabela
-    sql = '''CREATE TABLE sales_info(id int , 
-    transaction_id int , date datetime,  product_id int, seller_id int,
-    sale_value float, currency char(3));'''
-    
-    cursor.execute(sql) 
-    
-    # converter dataframe para sql 
-    data.to_sql('sales_info', conn, if_exists= 'append') 
-    
-    # Quantidade de linhas ingeridas no banco de dados de sua escolha é igual a quantidade de linhas originais
-    sql1='''select * from sales_info;'''
-    cursor.execute(sql1) 
-    for i in cursor.fetchall(): 
-        contador = contador + 1
-        print(i) 
+    try:
+        contador = 0
+        # criar conexão com db
+        conn_string = 'postgres://postgres:pass@127.0.0.1/Weather_Database'
         
-    if contador == data.shape[0]:
-        print("quantidade de linhas ingeridas de forma correta")
-        conn1.commit() 
-        conn1.close() 
-    else:
-        print("quantidade de linhas ingeridas de forma incorreta. A operação será revertida")
-        conn1.rollback()
-        conn1.close()
+        db = create_engine(conn_string) 
+        conn = db.connect() 
+        conn1 = psycopg2.connect( 
+        	database="Sales_Database", 
+        user='postgres', 
+        password='pass', 
+        host='127.0.0.1', 
+        port= '5432'
+        ) 
         
+        conn1.autocommit = False
+        cursor = conn1.cursor() 
+        
+        # deletar tabela se ela já existir
+        cursor.execute('drop table if exists sales_info') 
+        
+        #criar tabela
+        sql = '''CREATE TABLE sales_info(id int , 
+        transaction_id int , date datetime,  product_id int, seller_id int,
+        sale_value float, currency char(3));'''
+        
+        cursor.execute(sql) 
+        
+        # converter dataframe para sql 
+        data.to_sql('sales_info', conn, if_exists= 'append') 
+        
+        # Quantidade de linhas ingeridas no banco de dados de sua escolha é igual a quantidade de linhas originais
+        sql1='''select * from sales_info;'''
+        cursor.execute(sql1) 
+        for i in cursor.fetchall(): 
+            contador = contador + 1
+            print(i) 
+            
+        if contador == data.shape[0]:
+            print("quantidade de linhas ingeridas de forma correta")
+            conn1.commit() 
+            conn1.close() 
+        else:
+            print("quantidade de linhas ingeridas de forma incorreta. A operação será revertida")
+            conn1.rollback()
+            conn1.close()
+    except Exception as e:
+        # registrar a exceção
+        logging.exception(f'Ocorreu um erro: {e}')
+
         
 
         
@@ -132,7 +197,7 @@ def handler():
         sales_data_limpo = limpeza_dados(sales_data)
         sales_data_convertido = conversao(sales_data_limpo, "sale_value", 0.75, "USD")
         
-        criar_tabela(sales_data_convertido)
+        #criar_tabela(sales_data_convertido)
         
         end_time = time.time()
         
--- test_script.py
+++ test_script.py
@@ -8,28 +8,18 @@ Created on Thu Nov 23 20:18:47 2023
     
 
 import pandas as pd
+import pytest 
+import logging 
+
 
 #cenário: base de dados com coluna com erro de digitação, números negativos em sale_value e valor que deveria ser data com um "-" a mais
 
-from script_sales_data import limpeza_dados
+from script_sales_data import check_unicidade, check_duplicidade, check_vazios, check_negativos, check_data, limpeza_dados, conversao
         
 
-   
-    
-# criar um dataframe com alguns dados sujos
-sales_data = pd.DataFrame({
-    "transactionn_id": [1, 2, 3, 4, 4, 5, 6, 7, 8, 9],
-    "date": ["2023-01--01", "2023-01-02", "2023-01-03", "2023-01-04", "2023-01-04", "2023-01-05", "2023-01-06", "2023-01-07", "2023-01-08", "2023-01-09"],
-    "product_id": [101, 102, 103, 104, 104, 105, 106, 107, 108, 109],
-    "seller_id": [11, 12, 13, 14, 14, 15, 16, 17, 18, 19],
-    "sale_value": [100, -200, 300, 400, 400, 500, 600, 700, 800, 900],
-    "currency": ["USD", "BRL", "FICT", "USD", "USD", "USD", "USD", "USD", "USD", "USD"]
-    
-    
-})
 
 # criar um dataframe esperado com os dados limpos
-sales_data_clean = pd.DataFrame({
+sales_data = pd.DataFrame({
     "transaction_id": [1, 2, 3, 4, 4, 5, 6, 7, 8, 9],
     "date": ["2023-01-01", "2023-01-02", "2023-01-03", "2023-01-04", "2023-01-04", "2023-01-05", "2023-01-06", "2023-01-07", "2023-01-08", "2023-01-09"],
     "product_id": [101, 102, 103, 104, 104, 105, 106, 107, 108, 109],
@@ -39,6 +29,83 @@ sales_data_clean = pd.DataFrame({
     
 })
 
+
+taxa_conversao = 0.2 
+currency = "USD"
+
+def test_check_unicidade(): 
+    # verifique se a função remove IDs de transação duplicados
+    sales_data_unico = check_unicidade(sales_data) 
+    assert sales_data_unico["transaction_id"].nunique() == sales_data_unico.shape[0] 
+    # check that the function returns a dataframe 
+    assert isinstance(sales_data_unico, pd.DataFrame) 
+
+    
+    
+    
+def test_check_duplicidade(): 
+    # verifique se a função remove linhas duplicadas
+    sales_data_duplicado = check_duplicidade(sales_data) 
+    assert sales_data_duplicado.shape[0] == sales_data.drop_duplicates().shape[0] 
+    # verifique se a função retorna um dataframe
+    assert isinstance(sales_data_duplicado, pd.DataFrame) 
+ 
+    
+    
+def test_check_vazios(): 
+    # crie um dataframe com valores ausentes
+    sales_data_vazio = sales_data.copy() 
+    sales_data_vazio.loc[0, "sale_value"] = None 
+    # verifique se a função remove linhas com valores ausentes
+    sales_data_sem_vazio = check_vazios(sales_data_vazio) 
+    assert sales_data_sem_vazio.shape[0] == sales_data.shape[0] - 1 
+    # verifique se a função retorna um dataframe
+    assert isinstance(sales_data_sem_vazio, pd.DataFrame) 
+  
+
+def test_check_negativos(): 
+    # crie um dataframe com valores negativos
+    sales_data_negativo = sales_data.copy() 
+    sales_data_negativo.loc[0, "sale_value"] = -100 
+  
+    # verifique se a função retorna um dataframe
+    assert isinstance(sales_data_negativo, pd.DataFrame) 
+  
+
+
+def test_check_data(): 
+    # verifique se a função converte a coluna de data em data e hora
+    sales_data_data = check_data(sales_data) 
+    assert pd.core.dtypes.common.is_datetime_or_timedelta_dtype(sales_data_data["date"]) 
+    #  verifique se a função retorna um dataframe
+    assert isinstance(sales_data_data, pd.DataFrame) 
+  
+    
+    
+    
+def test_limpeza_dados(): 
+    # verifique se a função aplica todas as etapas de limpeza
+    sales_data_limpo = limpeza_dados(sales_data) 
+    assert sales_data_limpo.shape[0] == sales_data.drop_duplicates().shape[0] - 1 
+    assert sales_data_limpo["transaction_id"].nunique() == sales_data_limpo.shape[0] 
+    assert sales_data_limpo["sale_value"].isna().sum() == 0 
+    assert sales_data_limpo["sale_value"].min() >= 0 
+    assert pd.core.dtypes.common.is_datetime_or_timedelta_dtype(sales_data_limpo["date"]) 
+    # verifique se a função retorna um dataframe
+    assert isinstance(sales_data_limpo, pd.DataFrame) 
+     
+    
+    
+def test_conversao(): 
+    # verifique se a função converte a coluna sale_value para outra moeda
+    sales_data_convertido = conversao(sales_data, "sale_value", taxa_conversao, currency) 
+    assert sales_data_convertido["sale_value"].equals(sales_data["sale_value"] * taxa_conversao) 
+    assert sales_data_convertido["currency"].unique() == [currency] 
+    #verifique se a função retorna um dataframe
+    assert isinstance(sales_data_convertido, pd.DataFrame) 
+   
+    
+    
 # criar uma função de teste para verificar se a função limpeza_dados retorna o dataframe esperado
 def test_limpeza_dados():
     # aplicar a função limpeza_dados no dataframe de exemplo
